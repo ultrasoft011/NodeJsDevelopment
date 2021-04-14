92ac68bd3fb098d64f258ad458cae2d2cc161047
Andrés Felipe <ultrasoft.011@gmail.com>
2021-04-13 19:22:23 -0500

    importing core modules

---
+++ notes-app/script.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+
+// fs.writeFileSync('notes.txt', 'My name is Andres.')
+
+// Challenge: Append a message to notes.txt
+//
+// 1. Use appendFileSync to append to the file
+    fs.appendFileSync('notes.txt', 'Im getting stronger every day')
+// 2. Run the script
+// 3. Check your work by opening the file and viewing the appended text
+//
+
