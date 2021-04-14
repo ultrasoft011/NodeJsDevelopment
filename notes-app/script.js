b9bd9969f463878beb6c15ec6a897e40c0e2193a
Andrés Felipe <ultrasoft.011@gmail.com>
2021-04-13 20:35:09 -0500

    importing own files

--- notes-app/script.js
+++ notes-app/script.js
@@ -1,12 +1,17 @@
-const fs = require('fs')
-
-// fs.writeFileSync('notes.txt', 'My name is Andres.')
-
-// Challenge: Append a message to notes.txt
+// Challenge: Define and use a function in a new file
 //
-// 1. Use appendFileSync to append to the file
-    fs.appendFileSync('notes.txt', ' Im getting stronger every day')
-// 2. Run the script
-// 3. Check your work by opening the file and viewing the appended text
+// 1. Create a new file called notes.js
+// 2. Create getNotes functions that returns "Your notes.."
+// 3. Exports getNotes function
+// 4. From app.js, load in and call the function printing message to console
 //
 
+const message = require('./notes.js')
+
+console.log(message());
+
+// const add = require('./utils.js')
+//
+// const sum = add(4, 2);
+//
+// console.log(sum)
---
+++ notes-app/utils.js
@@ -0,0 +1,9 @@
+console.log('utils.js');
+
+const name = 'Felipe'
+
+const add = function (x, y) {
+    return x + y
+}
+
+module.exports = add
