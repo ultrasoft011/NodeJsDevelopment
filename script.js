20a386d80f49c65ef240356399c70f66ca45ba20
ultrasoft011 <ultrasoft.011@gmail.com>
2021-04-17 12:52:44 -0500

    title description, demandOption and type

--- script.js
+++ script.js
@@ -25,8 +25,15 @@ const read = require('./notes.js')
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: function () {
-        console.log('Adding a note')
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Title: ' + argv.title)
     }
 })
 
@@ -63,11 +70,7 @@ yargs.command({
 
 // Create add, remove, read and list commands
 
-
-console.log(yargs.argv)
-
-
-
+yargs.parse() 
 
 // if (command === 'add') {
 //     console.log('Adding note!')
